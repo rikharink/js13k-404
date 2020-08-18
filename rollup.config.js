ab8218f1adc75f47e1165f648143faaf47fd8f47
Rik Harink <rikharink@telluric.dev>
2020-08-18 23:21:32 +0200

    fixed dist build

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,58 @@ import packageOutput from "./plugins/rollup-plugin-package-js13k";
 
 const env = process.env.NODE_ENV || "development";
 
+let plugins = [
+  glslify({
+    compress: true,
+  }),
+  image(),
+  postcss({
+    extract: true,
+    minimize: true,
+    path: "./",
+    plugins: [],
+  }),
+  typescript(),
+  terser({
+    compress: {
+      passes: 4,
+      unsafe: true,
+      unsafe_arrows: true,
+      unsafe_comps: true,
+      unsafe_math: true,
+    },
+    ecma: 8,
+    mangle: {
+      properties: false,
+    },
+  }),
+  inline({
+    title: "js13k-template",
+    canvasId: "game",
+    template: defaultTemplate,
+    sourcemap: "main.js.map",
+  }),
+  packageOutput({
+    name: "js13k-template",
+    directory: "dist",
+    include: ["index.html"],
+    notify: env === "development",
+  }),
+];
+
+if (env === "development") {
+  plugins.push(
+    dev({
+      dirs: ["dist"],
+    })
+  );
+  plugins.push(
+    livereload({
+      watch: "dist",
+    })
+  );
+}
+
 export default {
   input: "src/main.ts",
   output: [
@@ -22,48 +74,5 @@ export default {
       plugins: [],
     },
   ],
-  plugins: [
-    glslify({
-      compress: true,
-    }),
-    image(),
-    postcss({
-      extract: true,
-      minimize: true,
-      path: "./",
-      plugins: [],
-    }),
-    typescript(),
-    terser({
-      compress: {
-        passes: 4,
-        unsafe: true,
-        unsafe_arrows: true,
-        unsafe_comps: true,
-        unsafe_math: true,
-      },
-      ecma: 8,
-      mangle: {
-        properties: false,
-      },
-    }),
-    inline({
-      title: "js13k-template",
-      canvasId: "game",
-      template: defaultTemplate,
-      sourcemap: "main.js.map",
-    }),
-    packageOutput({
-      name: "js13k-template",
-      directory: "dist",
-      include: ["index.html"],
-      notify: env === "development",
-    }),
-    dev({
-      dirs: ["dist"],
-    }),
-    livereload({
-      watch: "dist",
-    }),
-  ],
+  plugins: plugins,
 };
